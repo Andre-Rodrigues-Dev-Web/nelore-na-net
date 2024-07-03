1bafaa285c849bd72cfefc42d33824ca8bdcdc48
André Rodrigues <79713707+Andre-Rodrigues-Dev-Web@users.noreply.github.com>
2024-07-03 15:05:53 -0300

    feat: adicionados itens de menu

--- nelore-na-net/src/Shared/Components/Menu/index.jsx
+++ nelore-na-net/src/Shared/Components/Menu/index.jsx
@@ -1,47 +1,59 @@
-import { Link } from 'react-router-dom'
-import NeloreNaNet from '../../../assets/imgs/logo-nelore-na-net.png'
-import { ButtonContainer, ContainerLinks, ContainerMenu, ContentMenu, ListMenu, LogoNeloreNaNet } from './style'
+import { Link } from "react-router-dom";
+import NeloreNaNet from "../../../assets/imgs/logo-nelore-na-net.png";
+import {
+  ButtonContainer,
+  ContainerLinks,
+  ContainerMenu,
+  ContentMenu,
+  ListMenu,
+  LogoNeloreNaNet,
+} from "./style";
 
 const Menu = () => {
-    return (
-        <ContainerMenu>
-            <ContentMenu>
-                <Link to="/">
-                    <LogoNeloreNaNet src={NeloreNaNet} alt="Logo da Nelore na Net" />
-                </Link>
-                <ButtonContainer>
-                    <button>
-                        <span></span>
-                        <span></span>
-                        <span></span>
-                    </button>
-                </ButtonContainer>
-                <ContainerLinks>
-                    <ListMenu>
-                        <li>
-                            <Link to="/">Home</Link>
-                        </li>
-                        <li>
-                            <Link to="/historia">Conheça a raça</Link>
-                        </li>
-                        <li>
-                            <Link to="/cuidados">Cuidados</Link>
-                        </li>
-                        <li>
-                            <Link to="/alimentacao">Alimentação</Link>
-                        </li>
-                        <li>
-                            <Link to="/reproducao">Reprodução</Link>
-                        </li>
-                        <li>
-                            <Link to="/galeria">Galeria</Link>
-                        </li>
-                        
-                    </ListMenu>
-                </ContainerLinks>
-            </ContentMenu>
-        </ContainerMenu>
-    )
-}
+  return (
+    <ContainerMenu>
+      <ContentMenu>
+        <Link to="/">
+          <LogoNeloreNaNet src={NeloreNaNet} alt="Logo da Nelore na Net" />
+        </Link>
+        <ButtonContainer>
+          <button>
+            <span></span>
+            <span></span>
+            <span></span>
+          </button>
+        </ButtonContainer>
+        <ContainerLinks>
+          <ListMenu>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/galeria">Mercado Nelore</Link>
+            </li>
+            <li>
+              <Link to="/historia">Conheça a raça</Link>
+            </li>
+            <li>
+              <Link to="/cuidados">Cuidados</Link>
+            </li>
+            <li>
+              <Link to="/alimentacao">Alimentação</Link>
+            </li>
+            <li>
+              <Link to="/reproducao">Reprodução</Link>
+            </li>
+            <li>
+              <Link to="/galeria">Galeria</Link>
+            </li>
+            <li>
+              <Link to="/galeria">Agenda de Eventos</Link>
+            </li>
+          </ListMenu>
+        </ContainerLinks>
+      </ContentMenu>
+    </ContainerMenu>
+  );
+};
 
 export default Menu;
