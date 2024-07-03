7d6e463eab15944026b099e23bb1a319b6522a1c
André Rodrigues <79713707+Andre-Rodrigues-Dev-Web@users.noreply.github.com>
2024-07-03 15:24:29 -0300

    fix: adicionado evento

--- nelore-na-net/src/Shared/Components/Sidebar/index.jsx
+++ nelore-na-net/src/Shared/Components/Sidebar/index.jsx
@@ -8,6 +8,7 @@ const Sidebar = () => {
         <BovineQuotes />
         <h2>Arquivos</h2>
         <h2>Publicidade</h2>
+        <img src="https://d12ceyx7nhfnql.cloudfront.net/auctions/banners/000/002/023/medium/BANNER_PERFIL.png?1720020236" alt="evento liquidação Nelore Celo" />
        </ContainerSidebar>
     )
 }
